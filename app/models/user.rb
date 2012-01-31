0b93b0e3a81958bd1f7893d5fb7315f7b57661da
Joe Woodward <woodward.jp@gmail.com>
2012-01-31 18:44:12 +0000

    add tests for user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  describe '
+  context 'on create' do
+    it 'should verify presence of a first name' do
+      FactoryGirl.build(:user, :first_name => '').should_not be_valid
+    end
+    it 'should verify presence of a last name' do
+      FactoryGirl.build(:user, :last_name => '').should_not be_valid
+    end
+    it 'should verify presence of an email' do
+      FactoryGirl.build(:user, :email => '').should_not be_valid
+    end
+    it 'should verify presence of a password' do
+      FactoryGirl.build(:user, :password => '').should_not be_valid
+    end
+    it 'should verify presence of a password confirmation' do
+      FactoryGirl.build(:user, :password_confirmation => '').should_not be_valid
+    end
+    it 'should create a valid user' do
+      FactoryGirl.build(:user).should be_valid
+    end
+  end
 end
