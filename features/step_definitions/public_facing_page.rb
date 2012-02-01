89c1c2a0ef6e2630d1b13a83011fce452a9cdb93
Joe Woodward <woodward.jp@gmail.com>
2012-02-01 12:38:29 +0000

    added test

--- features/step_definitions/public_facing_page.rb
+++ features/step_definitions/public_facing_page.rb
@@ -23,16 +23,16 @@ Given /^I am on the "([^"]*)" page$/ do |page|
 end
 
 When /^I fill in my details correctly and submit them$/ do
-  fill_in('First Name', :with => 'Steve')
-  fill_in('Last Name', :with => 'Jobs')
+  fill_in('First name', :with => 'Steve')
+  fill_in('Surname', :with => 'Jobs')
   fill_in('Email', :with => 'some.unknown.email.address@gmail.com')
   fill_in('Password', :with => 'password')
-  fill_in('Password Confirmation', :with => 'password')
+  fill_in('Password confirmation', :with => 'password')
   find_button('Continue').click
 end
 
 Then /^I should have an account$/ do
-  pending # express the regexp above with the code you wish you had
+  User.find_by_email('some.unknown.email.address@gmail.com').should_not be_nil
 end
 
 Then /^I should be logged in$/ do
