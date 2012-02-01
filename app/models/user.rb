f354e2948fd1ee943723dfc67167f894f43328a9
Joe Woodward <woodward.jp@gmail.com>
2012-02-01 20:36:15 +0000

    add friendly_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  extend FriendlyId
+  friendly_id :full_name, use: :slugged
 
-  attr_accessible :first_name, :last_name, :email, :password_confirmation, :password
+  before_save :format_attributes
+
+  attr_accessible :first_name, :last_name, :email, :password_confirmation, :password, :full_name
 
   validates_presence_of :first_name
   validates_presence_of :last_name
@@ -12,4 +16,11 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
 
   validates_uniqueness_of :email
+
+  def format_attributes
+    self.email = self.email.downcase
+    self.first_name = self.first_name.titleize
+    self.last_name = self.last_name.titleize
+    self.full_name = "#{self.first_name} #{self.last_name}"
+  end
 end
--- db/migrate/001_sorcery_core.rb
+++ db/migrate/001_sorcery_core.rb
@@ -3,6 +3,7 @@ class SorceryCore < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name,       :default => nil  # if you use another field as a username, for example email, you can safely remove this field.
       t.string :last_name,        :default => nil
+      t.string :full_name,         :default => nil
       t.string :email,            :null => false # if you use this field as a username, you might want to make it :null => false.
       t.string :crypted_password, :default => nil
       t.string :salt,             :default => nil
