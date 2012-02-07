498a4d43c8cb80228f60db6422e19c89a84bcdf4
Joe Woodward <woodward.jp@gmail.com>
2012-02-07 11:17:53 +0000

    route to admin root path after login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -6,7 +6,7 @@ class Admin::SessionsController < Admin::ApplicationController
   def create
     user = login(params[:email], params[:password], params[:remember_me])
     if user
-      redirect_to admin_account_path(current_user), :notice => "Logged in as #{ current_user.email }"
+      redirect_to admin_accounts, :notice => "Logged in as #{ current_user.email }"
     else
       redirect_to admin_login_path, :notice => 'Email or password was invalid'
     end
