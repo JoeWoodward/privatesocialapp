a4d0a212bddac50e60b3318e9966d37c767c7a67
Ben Woodward <woodward.ben@gmail.com>
2012-02-11 14:17:28 +0000

    add @orientation variable for use in lalyout

--- app/controllers/account/account_controller.rb
+++ app/controllers/account/account_controller.rb
@@ -1,3 +1,20 @@
 class Account::AccountController < ApplicationController
+  layout 'account'
 
+  before_filter :orientation
+
+  private
+
+  #override this variable inside controllers or actions
+  #change value to 'vertical' .. obviously!
+
+  #css defitions exist for .horizontal-wrapper and 
+  #.vertical-wrapper
+  def orientation
+
+    # set default layout to vertical
+    # @orientation is used in the layout..
+    # HAML: %div{class: "#{@orientation}-wrapper"}
+    @orientation = 'vertical'
+  end
 end
