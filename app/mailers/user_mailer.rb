994eb5a008a474315b7ba5c53d5735db4e3673cc
Joe Woodward <woodward.jp@gmail.com>
2012-02-16 16:08:45 +0000

    correct route for password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "48 Harley Street Notifier <48harleystreet.notifier@gmail.com>"
   def reset_password_email(user)
     @user = user
-    @url  = "http://178.79.159.74/password_resets/#{user.reset_password_token}/edit"
+    @url  = "http://178.79.159.74/password-reset/#{user.reset_password_token}/edit"
     mail(:to => "#{user.full_name}<#{user.email}>",
          :subject => "Password reset confirmation from 48 Harley Street")
   end
