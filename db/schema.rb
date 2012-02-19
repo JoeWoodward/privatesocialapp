e8a03e02f5c91970433a92187ca72dd323fac5e3
Ben Woodward <woodward.ben@gmail.com>
2012-02-19 20:42:17 +0000

    rename migration due to conflict in names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 17) do
+ActiveRecord::Schema.define(:version => 18) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id"
