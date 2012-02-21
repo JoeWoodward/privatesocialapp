4befb53d6cca3eba8a45134f4b003d3abdba2fad
Joe Woodward <woodward.jp@gmail.com>
2012-02-21 11:22:55 +0000

    fix event word wrap bug and styles event index + show

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,7 @@ module EventsHelper
     if current_user.events.include?(event)
       link_to 'Cancel RSVP', cancel_event_rsvp_path(event), class: 'button'
     else
-      link_to 'RSVP', rsvp_event_path(event), :confirm => "This will add the event to 'your events' within the account area", class: 'button'
+      link_to 'RSVP', rsvp_event_path(event), class: 'button'
     end
   end
 end
