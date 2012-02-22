0a069ed0ca4a83b37a966d83da501c3d21dd40fc
Joe Woodward <woodward.jp@gmail.com>
2012-02-22 21:25:45 +0000

    add subscription changed and billing date changed webhooks

--- app/controllers/chargify/hooks_controller.rb
+++ app/controllers/chargify/hooks_controller.rb
@@ -52,11 +52,31 @@ class Chargify::HooksController < ApplicationController
   end
 
   def billing_date_change
+    begin
+      @user = User.find_by_token(@subscription.customer.reference)
+      @user.state = @subscription.state
+      @user.subscription_billing_date = @subscription.current_period_ends_at
+      @user.save(:validate => false)
       render :nothing => true, :status => 200
+    rescue Exception => e
+      render :nothing => true, :status => 422 and return
+    end
   end
 
   def subscription_state_change
+    begin
+      puts @subscription.customer.email
+      @user = User.find_by_email(@subscription.customer.email)
+      puts @user.full_name
+      @user.state = @subscription.state
+      puts @user.state
+      @user.subscription_billing_date = @subscription.current_period_ends_at
+      @user.save
       render :nothing => true, :status => 200
+    rescue Exception => e
+      puts e
+      render :nothing => true, :status => 422 and return
+    end
   end
 
   def subscription_product_change
@@ -70,7 +90,7 @@ class Chargify::HooksController < ApplicationController
     end
 
 
-    unless Digest::MD5.hexdigest(self.site_key + request.raw_post) == params[:signature]
+    unless Digest::MD5.hexdigest(site_key + request.raw_post) == params[:signature]
       render :nothing => true, :status => :forbidden
      end
   end
@@ -85,12 +105,11 @@ class Chargify::HooksController < ApplicationController
     end
   end
 
-  private
-  def self.chargify_config
+  def chargify_config
     YAML::load_file(File.join(Rails.root, 'config', 'chargify.yml'))
   end
 
-  def self.site_key
-    self.chargify_config[Rails.env]['site_key']
+  def site_key
+    chargify_config[Rails.env]['site_key']
   end
 end
