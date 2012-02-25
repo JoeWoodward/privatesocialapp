c4a8c430e0423d903000101d4b2d1bbff95ebd86
Joe Woodward <woodward.jp@gmail.com>
2012-02-25 17:31:48 +0000

    add partner model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20) do
+ActiveRecord::Schema.define(:version => 21) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id"
@@ -94,6 +94,11 @@ ActiveRecord::Schema.define(:version => 20) do
   end
 
   add_index "offers", ["slug"], :name => "index_offers_on_slug"
+
+  create_table "partners", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
