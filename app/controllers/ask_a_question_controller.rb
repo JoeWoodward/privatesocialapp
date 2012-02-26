9d880da42acf0974c02ab5c8f91bd845357aba98
Ben Woodward <woodward.ben@gmail.com>
2012-02-26 19:38:44 +0000

    adjust spacing / css for ask question form

--- app/controllers/ask_a_question_controller.rb
+++ app/controllers/ask_a_question_controller.rb
@@ -1,13 +1,17 @@
 class AskAQuestionController < ApplicationController
   skip_before_filter :require_login
-  before_filter :orientation
   before_filter :check_message, :only => :create
 
   def new
+    @question = ''
   end
 
   def create
-    AskAQuestionMailer.the_users_question(current_user.full_name, current_user.email, params[:question]).deliver
+    AskAQuestionMailer.the_users_question(
+      current_user.full_name, current_user.email,
+      params[:question]
+    ).deliver
+
     redirect_to faqs_path, :notice => 'Your question has been submitted, we will answer as soon as possible'
   end
 
