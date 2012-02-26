82ebc580390ae4d227ed0bff9d7fda0633064836
Ben Woodward <woodward.ben@gmail.com>
2012-02-26 17:44:11 +0000

    fix remember_me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   skip_before_filter :is_user_authorised
 
   def create
-    @user = login(params[:user][:email], params[:user][:password], params[:user][:remember_me])
+    @user = login(params[:user][:email], params[:user][:password], params[:remember])
     if @user
       redirect_to your_details_path, :notice => "Logged in as #{ current_user.email }"
     else
