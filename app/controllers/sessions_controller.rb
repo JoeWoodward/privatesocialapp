e4b60fe115cdd51e16d846d3f21f112e0928c75c
Ben Woodward <woodward.ben@gmail.com>
2012-02-26 16:12:30 +0000

    add css for sign up page. what a headache

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   skip_before_filter :is_user_authorised
 
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
-    if user
+    @user = login(params[:email], params[:password], params[:remember_me])
+    if @user
       redirect_to your_details_path, :notice => "Logged in as #{ current_user.email }"
     else
       redirect_to root_path, :notice => 'Email or password was invalid'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_filter :require_login
-  before_filter :orientation
   skip_before_filter :is_user_authorised
 
   def home
     redirect_to your_details_path if current_user
+    @user = User.new
+    @section_style = 'half-width-translucent'
+    @content_holder = 'your-content sign-up'
   end
 
   def privacy
