cadf029c130eb18ed85a9f0f2f34fc47476da033
Joe Woodward <woodward.jp@gmail.com>
2012-02-28 23:35:20 +0000

    add config for ask a question mailer

--- app/mailers/ask_a_question_mailer.rb
+++ app/mailers/ask_a_question_mailer.rb
@@ -1,5 +1,5 @@
 class AskAQuestionMailer < ActionMailer::Base
-  default to: "facebook.spotify@gmail.com",
+  default to: "#{ENV=['ASK_A_DOCTOR_EMAIL_ADDRESS']}",
           from: "48 Harley Street Notifier <48harleystreet.notifier@gmail.com>"
   def the_users_question(user_full_name, user_email, question)
     @name = user_full_name
