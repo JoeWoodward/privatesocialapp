b6c6b7a85280d99e0531898f987a4e1d7aee9a78
Joe Woodward <woodward.jp@gmail.com>
2012-03-13 12:11:03 +0000

    change shape of partner logo

--- app/uploaders/partner_logo_uploader.rb
+++ app/uploaders/partner_logo_uploader.rb
@@ -14,6 +14,6 @@ class PartnerLogoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :logo do
-    process :resize_to_fill => [60, 60]
+    process :resize_to_fit => [50, 2000]
   end
 end
